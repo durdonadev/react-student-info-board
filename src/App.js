fdfcdc94335a0370bb2332cf6742dd52bc48e8bc
Durdona Ibragimova <durdona.dev@gmail.com>
2024-03-07 12:57:16 -0500

    Create Student data viewer board

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { students } from "./data";
+import { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    const [selectedStudent, setSelectedStudent] = useState();
+
+    const showStudentInfo = (student) => {
+        setSelectedStudent(student);
+        console.log(student);
+    };
+
+    return (
+        <div className="content">
+            <h1>Students Data</h1>
+            <div className="container">
+                <div className="student-names">
+                    <h3>Students</h3>
+                    <ul>
+                        {students.map((student, idx) => {
+                            return (
+                                <li
+                                    key={idx}
+                                    className="student"
+                                    onClick={() => showStudentInfo(student)}
+                                >
+                                    {student.name} {student.last_name}
+                                </li>
+                            );
+                        })}
+                    </ul>
+                </div>
+                <div className="student-info">
+                    <h3>Student's Info</h3>
+                    {selectedStudent && (
+                        <div>
+                            <p>
+                                Full Name: {selectedStudent.name}{" "}
+                                {selectedStudent.last_name}
+                            </p>
+                            <p>Age: {selectedStudent.age}</p>
+                            <p>City: {selectedStudent.city}</p>
+                            <p>Phone Number: {selectedStudent.phone_number}</p>
+                            <p>
+                                Number of Classes took:{" "}
+                                {selectedStudent.number_of_classes_took}
+                            </p>
+                        </div>
+                    )}
+                </div>
+            </div>
+        </div>
+    );
 }
-
-export default App;
