bf2476a70bb1d7506f0b89df2991eb0e7f6b9586
Durdona Ibragimova <durdona.dev@gmail.com>
2024-03-07 12:58:21 -0500

    Add students data

---
+++ src/data.js
@@ -0,0 +1,92 @@
+export const students = [
+    {
+        name: "Sarah",
+        last_name: "Roman",
+        age: 38,
+        city: "Port Ashley",
+        phone_number: "418-289-6445x0385",
+        number_of_classes_took: 8,
+        uuid: "622d6011-6bd4-4ee0-95d3-ddefc200dc93"
+    },
+    {
+        name: "Jason",
+        last_name: "Martin",
+        age: 27,
+        city: "Millerport",
+        phone_number: "(100)835-9983",
+        number_of_classes_took: 18,
+        uuid: "cc50b430-2e08-4be0-9693-30e7bae51403"
+    },
+    {
+        name: "Brian",
+        last_name: "Rodriguez",
+        age: 40,
+        city: "South Theresa",
+        phone_number: "001-367-092-2926x76534",
+        number_of_classes_took: 7,
+        uuid: "2da6a3a8-a496-4773-8feb-9a42fc2250e3"
+    },
+    {
+        name: "Sara",
+        last_name: "Brown",
+        age: 29,
+        city: "New Matthewhaven",
+        phone_number: "(173)724-5835x14631",
+        number_of_classes_took: 1,
+        uuid: "86aed403-9d6f-4ec4-b8c7-2ecaf6b63d84"
+    },
+    {
+        name: "Jeffrey",
+        last_name: "Knight",
+        age: 44,
+        city: "Karenchester",
+        phone_number: "+1-470-348-1791x72582",
+        number_of_classes_took: 7,
+        uuid: "422d0843-6f40-4440-8f52-47db2bcd9bca"
+    },
+    {
+        name: "Annette",
+        last_name: "Carter",
+        age: 49,
+        city: "Jaredshire",
+        phone_number: "8912903396",
+        number_of_classes_took: 5,
+        uuid: "1731a1dc-4c84-45cf-9ce9-58f87efa25e6"
+    },
+    {
+        name: "Manuel",
+        last_name: "Hubbard",
+        age: 56,
+        city: "Lake Suzannemouth",
+        phone_number: "(235)061-2098",
+        number_of_classes_took: 19,
+        uuid: "6bb3e9a2-8489-430c-88da-7fcf1a04ad46"
+    },
+    {
+        name: "Elizabeth",
+        last_name: "Moran",
+        age: 31,
+        city: "Brookechester",
+        phone_number: "706.345.3379",
+        number_of_classes_took: 5,
+        uuid: "68dd0ebc-e44d-47d5-a463-e1ebb4cec0c2"
+    },
+    {
+        name: "Samantha",
+        last_name: "Odom",
+        age: 31,
+        city: "Port Shawn",
+        phone_number: "628.275.2184x5749",
+        number_of_classes_took: 11,
+        uuid: "28651289-1074-4aef-9640-5a49d12b2fc5"
+    },
+    {
+        name: "Nicole",
+        last_name: "White",
+        age: 20,
+        city: "Bethfurt",
+        phone_number: "001-658-334-8046x51835",
+        number_of_classes_took: 17,
+        uuid: "d9514880-f10e-4bbc-9654-74a626841796"
+    }
+];
